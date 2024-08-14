bb6942a474d591816b0d4e7ab21e6a7d306c46d5
Антон Фролов <fr.anton@mail.ru>
2024-08-14 15:34:10 +0300

    CLI - Add back command to answers variants

--- packages/cli/cli/dialog.js
+++ packages/cli/cli/dialog.js
@@ -23,11 +23,11 @@ const askQuestion = async (question) => new Promise((resolve) => {
 });
 
 const getProcessedQuestion = (data, pathHistory) => {
-    const header = `${logger.getInclinedGrayText(pathHistory)}\n${logger.getBoldText(data.question)}`;
+    const header = `${logger.getInclinedGrayText(`/${pathHistory.join('/')}`)}\n${logger.getBoldText(data.question)}`;
     return `${header}\n${data.answers.reduce((result, item) => {
         const number = item.id;
         return `${result}${logger.getGrayText(number)} ${item.value}${number === '1' ? logger.getGrayText(' (default)') : ''}\n`;
-    }, '')}> `;
+    }, '')}${logger.getGrayText('0 back')}\n> `;
 };
 
 const getAnswerFromDialog = async (data, pathHistory) => {
@@ -35,26 +35,33 @@ const getAnswerFromDialog = async (data, pathHistory) => {
     let result;
 
     const answer = await askQuestion(question);
+    const isMatchBack = answer === '0' || answer === 'back';
 
-    data.answers.forEach((item) => {
-        const isMatchIdentifier = answer === item.id;
-        const isMatchWord = answer === item.value;
-
-        if (isMatchIdentifier || isMatchWord) {
-            result = item;
+    if (isMatchBack) {
+        result = {
+            command: pathHistory.slice(0, pathHistory.length - 1).join(' ')
         }
-    });
+    } else {
+        data.answers.forEach((item) => {
+            const isMatchIdentifier = answer === item.id;
+            const isMatchWord = answer === item.value;
+
+            if (isMatchIdentifier || isMatchWord) {
+                result = item;
+            }
+        });
+    }
 
     return result || (answer ? null : data.answers[0]);
 };
 
 const getCommandFromDialog = async (data) => {
-    let pathHistory = '/cli';
+    let pathHistory = [ 'cli' ];
     let command;
 
     const processDialog = async (data) => {
         if (data.answers) {
-            pathHistory = data.value ? `${pathHistory}/${data.value}` : pathHistory;
+            data.value && pathHistory.push(data.value);
             const result = await getAnswerFromDialog(data, pathHistory);
             result ? await processDialog(result) : logger.logWrongChooseError();
         } else {
